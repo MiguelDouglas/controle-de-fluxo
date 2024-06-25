fe5f954d3fed972409cd350845294cb79d7071f6
MiguelDouglas <miguel.douglas.silva@gmail.com>
2024-06-25 04:49:07 -0300

    If Else com "?" e ":"

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -2,15 +2,13 @@ public class ResultadoEscolar {
     
     public static void main(String[] args) {
         
-       int nota = 4;
-       
-       if(nota >= 7)
-        System.out.println("Aprovado");
+        int nota = 8;
 
-        else if(nota>=5 && nota <7)
-        System.out.println("Prova de Recuperação");
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
 
-       else
-        System.out.println("Reprovado");
-    }
+		System.out.println(resultado);
+
+
+   }
 }
+    
